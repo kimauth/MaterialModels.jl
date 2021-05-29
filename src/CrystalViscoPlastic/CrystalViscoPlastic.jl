fff9c08078248780ab602559e633208398100cfd
kimauth <auth@chalmers.se>
2021-05-29 16:30:42 +0200

    performance enhancements CrystalPlasticity

--- src/CrystalViscoPlastic/CrystalViscoPlastic.jl
+++ src/CrystalViscoPlastic/CrystalViscoPlastic.jl
@@ -30,6 +30,7 @@ struct CrystalViscoPlastic{S} <: AbstractMaterial
     # precomputed elastic tangent tensor and hardening matrix
     Eᵉ::SymmetricTensor{4,3,Float64,36}
     H::Matrix{Float64}
+    MS::SVector{S,Tensor{2,3,Float64,9}}
     # store information about slip sytems
     nslipsystems::Int
     slipsystems::Vector{NTuple{2, Vec{3}}}
@@ -38,8 +39,8 @@ struct CrystalViscoPlastic{S} <: AbstractMaterial
         Eᵉ = elastic_tangent_3D(E, ν)
         nslipsystems = length(slipsystems)
         H = H_iso*(q*ones(nslipsystems, nslipsystems) + (1-q)*diagm(ones(nslipsystems)))
-
-        return new{nslipsystems}(E, ν, τ_y, H_iso, H_kin, q, α_∞, t_star, σ_c, m, Eᵉ, H, nslipsystems, slipsystems)
+        MS = SVector{nslipsystems,Tensor{2,3,Float64,9}}([slipsystems[i][1] ⊗ slipsystems[i][2] for i=1:nslipsystems])
+        return new{nslipsystems}(E, ν, τ_y, H_iso, H_kin, q, α_∞, t_star, σ_c, m, Eᵉ, H, MS, nslipsystems, slipsystems)
     end
 end
 
@@ -49,12 +50,12 @@ CrystalViscoPlastic(; E, ν, τ_y, H_iso, H_kin, q, α_∞, t_star, σ_c, m, sli
 # to do: maybe parametrize with nslipsystems and use tuples?
 struct CrystalViscoPlasticState{dim,T,M,S} <: AbstractMaterialState
     σ::SymmetricTensor{2,dim,T,M}
-    κ::Vector{Float64}
-    α::Vector{Float64}
-    μ::Vector{Float64}
+    κ::SVector{S, Float64}
+    α::SVector{S, Float64}
+    μ::SVector{S, Float64}
 end
 
-Base.zero(::Type{CrystalViscoPlasticState{dim,T,M,S}}) where {dim,T,M,S} = CrystalViscoPlasticState{dim,T,M,S}(zero(SymmetricTensor{2,dim,T,M}), zeros(T, S), zeros(T, S), zeros(T, S))
+Base.zero(::Type{CrystalViscoPlasticState{dim,T,M,S}}) where {dim,T,M,S} = CrystalViscoPlasticState{dim,T,M,S}(zero(SymmetricTensor{2,dim,T,M}), SVector{S,T}(zeros(T, S)), SVector{S,T}(zeros(T, S)), SVector{S,T}(zeros(T, S)))
 initial_material_state(::CrystalViscoPlastic{S}) where S = zero(CrystalViscoPlasticState{3,Float64,6,S})
 
 function get_cache(m::CrystalViscoPlastic)
@@ -71,40 +72,41 @@ end
 # TODO: Should Residuals have a Type Parameter N for the number of scalar equations?
 struct ResidualsCrystalViscoPlastic{S, T}
     σ::SymmetricTensor{2,3,T,6}
-    κ::Vector{T}
-    α::Vector{T}
-    μ::Vector{T}
-    function ResidualsCrystalViscoPlastic(σ::SymmetricTensor{2,3,T,6}, κ::Vector{T}, α::Vector{T}, μ::Vector{T}) where {T}
-        S = length(κ)
-        length(α) == S || error("$S slipsystems for κ, but $(length(α)) slipsystems for α handed over to Residuals{CrystalViscoPlastic}.")
-        length(μ) == S || error("$S slipsystems for κ, but $(length(μ)) slipsystems for μ handed over to Residuals{CrystalViscoPlastic}.")
-        new{S,T}(σ, κ, α, μ)
-    end
+    κ::SVector{S, T}
+    α::SVector{S, T}
+    μ::SVector{S, T}
+    # function ResidualsCrystalViscoPlastic(σ::SymmetricTensor{2,3,T,6}, κ::MVector{S, T}, α::MVector{S, T}, μ::MVector{S, T}) where {S, T}
+    #     # S = length(κ)
+    #     # length(α) == S || error("$S slipsystems for κ, but $(length(α)) slipsystems for α handed over to Residuals{CrystalViscoPlastic}.")
+    #     # length(μ) == S || error("$S slipsystems for κ, but $(length(μ)) slipsystems for μ handed over to Residuals{CrystalViscoPlastic}.")
+    #     new{S,T}(σ, κ, α, μ)
+    # end
 end
 
 get_n_scalar_equations(::CrystalViscoPlastic{S}) where S = 6 + 3S
-get_residual_type(::CrystalViscoPlastic{S}) where S = ResidualsCrystalViscoPlastic{S}
+# get_residual_type(::CrystalViscoPlastic{S}) where S = ResidualsCrystalViscoPlastic{S}
+Tensors.get_base(::Type{CrystalViscoPlastic{S}}) where S = ResidualsCrystalViscoPlastic{S} # needed for frommandel
 
 function Tensors.tomandel!(v::Vector{T}, r::ResidualsCrystalViscoPlastic{S, T}) where {S, T}
     M=6
     # TODO check vector length
     tomandel!(view(v, 1:M), r.σ)
-    v[M+1:M+S] = r.κ
-    v[M+S+1:M+2S] = r.α
-    v[M+2S+1:M+3S] = r.μ
+    v[M+1:M+S] = view(r.κ, :)
+    v[M+S+1:M+2S] = view(r.α, :)
+    v[M+2S+1:M+3S] = view(r.μ, :)
     return v
 end
 
 function Tensors.frommandel(::Type{ResidualsCrystalViscoPlastic{S}}, v::Vector{T}) where {S, T}
     M = 6
     σ = frommandel(SymmetricTensor{2,3}, view(v, 1:M))
-    κ = v[M+1:M+S]
-    α = v[M+S+1:M+2S]
-    μ = v[M+2S+1:M+3S]
+    κ = SVector{S,T}(view(v, M+1:M+S))
+    α = SVector{S,T}(view(v, M+S+1:M+2S))
+    μ = SVector{S,T}(view(v, M+2S+1:M+3S))
     return ResidualsCrystalViscoPlastic(σ, κ, α, μ)
 end
 
-function constitutive_driver(m::CrystalViscoPlastic{S}, Δε::SymmetricTensor{2,3,T,6}, state::CrystalViscoPlasticState{3},
+function material_response(m::CrystalViscoPlastic{S}, Δε::SymmetricTensor{2,3,T,6}, state::CrystalViscoPlasticState{3},
     Δt=1.0; cache=get_cache(m), options::Dict{Symbol, Any} = Dict{Symbol, Any}()) where {S, T}
 
     # set the current residual function that depends only on the variables
@@ -118,14 +120,15 @@ function constitutive_driver(m::CrystalViscoPlastic{S}, Δε::SymmetricTensor{2,
     # solve for variables x
     nlsolve_options = get(options, :nlsolve_params, Dict{Symbol, Any}(:method=>:newton))
     haskey(nlsolve_options, :method) || merge!(nlsolve_options, Dict{Symbol, Any}(:method=>:newton)) # set newton if the user did not supply another method
-    @timeit "nlsolve" result = NLsolve.nlsolve(cache, cache.x_f; nlsolve_options...)
+    result = NLsolve.nlsolve(cache, cache.x_f; nlsolve_options...)
     
     if result.f_converged
-        x = frommandel(ResidualsCrystalViscoPlastic{S}, result.zero)
+        x_mandel = result.zero::Vector{T}
+        x = frommandel(ResidualsCrystalViscoPlastic{S}, x_mandel)
         dRdx = cache.DF
         inv_J_σσ = frommandel(SymmetricTensor{4,3}, inv(dRdx))
         ∂σ∂ε = inv_J_σσ ⊡ m.Eᵉ
-        return x.σ, ∂σ∂ε, CrystalViscoPlasticState{3,T,6,S}(x.σ, x.κ, x.α, x.μ)
+        return x.σ, ∂σ∂ε, CrystalViscoPlasticState{3,T,6,S}(x.σ, SVector{S,T}(x.κ), SVector{S,T}(x.α), SVector{S,T}(x.μ))
     else
         error("Material model not converged. Could not find material state.")
     end
@@ -135,27 +138,29 @@ function residuals(vars::ResidualsCrystalViscoPlastic{S,T}, mat::CrystalViscoPla
     σ=vars.σ; κ=vars.κ; α=vars.α; μ=vars.μ
 
     temp_sum_σ = zero(Tensor{2,3})
-    Rκ = Vector{T}(undef, S)
-    Rα = Vector{T}(undef, S)
-    Rμ = Vector{T}(undef, S)
+    Rκ = MVector{S, T}(undef)
+    Rα = MVector{S, T}(undef)
+    Rμ = MVector{S, T}(undef)
     for i=1:S
-        m = mat.slipsystems[i][1]
-        s = mat.slipsystems[i][2]
-        ms_tensor = m ⊗ s
+        # m = mat.slipsystems[i][1]
+        # s = mat.slipsystems[i][2]
+        # ms_tensor = m ⊗ s
+        ms_tensor = mat.MS[i]
         τ = σ ⊡ ms_tensor
-        stress_sign = sign(τ - α[i])
-        Φ = norm(τ - α[i]) - (mat.τ_y + κ[i])
+        σ_red = τ - α[i]
+        stress_sign = sign(σ_red)
+        Φ = norm(σ_red) - (mat.τ_y + κ[i])
 
         # R_σ
         temp_sum_σ += μ[i] * ms_tensor * stress_sign
 
         # R_κ
-        temp_sum_κ = 0.0
-        for j=1:S
-            # H is symmetric, thus we can take columns instead of rows
-            temp_sum_κ += mat.H[j,i]*μ[i]
-        end
-        Rκ[i] = κ[i] - material_state.κ[i] - temp_sum_κ
+        # temp_sum_κ = 0.0
+        # for j=1:S
+        #     # H is symmetric, thus we can take columns instead of rows
+        #     temp_sum_κ += mat.H[j,i]*μ[i]
+        # end
+        Rκ[i] = κ[i] - material_state.κ[i] - dot(view(mat.H, :,i), μ)
 
         # R_α
         Rα[i] = α[i] - material_state.α[i] - μ[i]*mat.H_kin*(stress_sign-α[i]/mat.α_∞)
@@ -165,5 +170,41 @@ function residuals(vars::ResidualsCrystalViscoPlastic{S,T}, mat::CrystalViscoPla
     end
 
     Rσ = σ - material_state.σ - mat.Eᵉ ⊡ Δε + mat.Eᵉ ⊡ temp_sum_σ
-    return  ResidualsCrystalViscoPlastic(Rσ, Rκ, Rα, Rμ)
+    return  ResidualsCrystalViscoPlastic(Rσ, SVector{S,T}(Rκ), SVector{S,T}(Rα), SVector{S,T}(Rμ))
 end
+
+# # use StaticVectors everywhere
+# function residuals_var(vars::ResidualsCrystalViscoPlastic{S,T}, mat::CrystalViscoPlastic{S}, material_state::CrystalViscoPlasticState{3}, Δε, Δt) where {S,T}
+#     σ=vars.σ; κ=vars.κ; α=vars.α; μ=vars.μ
+
+#     temp_sum_σ = zero(Tensor{2,3})
+#     for i=1:S
+#         m = mat.slipsystems[i][1]
+#         s = mat.slipsystems[i][2]
+#         ms_tensor = m ⊗ s
+#         τ = σ ⊡ ms_tensor
+        
+#         stress_sign = sign(τ - α[i])
+#         Φ = norm(τ - α[i]) - (mat.τ_y + κ[i])
+
+#         # R_σ
+#         temp_sum_σ += μ[i] * ms_tensor * stress_sign
+
+#         # R_κ
+#         temp_sum_κ = 0.0
+#         for j=1:S
+#             # H is symmetric, thus we can take columns instead of rows
+#             temp_sum_κ += mat.H[j,i]*μ[i]
+#         end
+#         Rκ[i] = κ[i] - material_state.κ[i] - temp_sum_κ
+
+#         # R_α
+#         Rα[i] = α[i] - material_state.α[i] - μ[i]*mat.H_kin*(stress_sign-α[i]/mat.α_∞)
+
+#         # R_Φ
+#         Rμ[i] = Δt*(((Φ+abs(Φ))/2)/mat.σ_c)^mat.m - mat.t_star*μ[i]
+#     end
+
+#     Rσ = σ - material_state.σ - mat.Eᵉ ⊡ Δε + mat.Eᵉ ⊡ temp_sum_σ
+#     return  ResidualsCrystalViscoPlastic(Rσ, Rκ, Rα, Rμ)
+# end
