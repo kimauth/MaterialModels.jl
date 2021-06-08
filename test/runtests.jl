52ac90441f0ab6bf6205d091aaf8f53d335a1235
kimauth <auth@chalmers.se>
2021-06-08 16:54:29 +0200

    add runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,4 @@ using Rotations
 include("test_utils.jl")
 include("test_linear_elastic.jl")
 include("test_plastic.jl")
+include("test_wrappers.jl")
---
+++ test/test_wrappers.jl
@@ -0,0 +1,42 @@
+@testset "wrappers" begin
+    m = LinearElastic(E=200e3, ν=0.3)
+    state = initial_material_state(m)
+
+    ################################################
+    # stress wrapper
+    ################################################
+    # 1D
+    dim = MaterialModels.UniaxialStress{1}()
+    Δε = rand(SymmetricTensor{2,1})
+    σ, ∂σ∂ε, temp_state = material_response(dim, m, Δε, state)
+    @test ∂σ∂ε[1] ≈ m.E
+    @test σ ≈ m.E*Δε
+
+    # 2D
+    dim = MaterialModels.PlaneStress{2}()
+    Δε = rand(SymmetricTensor{2,2})
+    σ, ∂σ∂ε, temp_state = material_response(dim, m, Δε, state)
+    @test σ ≈ ∂σ∂ε ⊡ Δε
+    @test tovoigt(∂σ∂ε) ≈ m.E/(1-m.ν^2)*[1 m.ν 0; m.ν 1 0; 0 0 (1-m.ν)/2]
+
+    ################################################
+    # strain wrapper
+    ################################################
+    # 1D
+    dim = MaterialModels.UniaxialStrain{1}()
+    Δε = rand(SymmetricTensor{2,1})
+    σ, ∂σ∂ε, temp_state = material_response(dim, m, Δε, state)
+    Δε_3D = SymmetricTensor{2,3}((i,j)-> i==1 && j==1 ? Δε[1,1] : 0.0)
+    σ_3D, ∂σ∂ε_3D, temp_state = material_response(m, Δε_3D, state)
+    @test σ[1,1] == σ_3D[1,1]
+    @test ∂σ∂ε[1,1,1,1] == ∂σ∂ε_3D[1,1,1,1]
+
+    # 2D
+    dim = MaterialModels.PlaneStrain{2}()
+    Δε = rand(SymmetricTensor{2,2})
+    σ, ∂σ∂ε, temp_state = material_response(dim, m, Δε, state)
+    @test σ ≈ ∂σ∂ε ⊡ Δε
+    @test tovoigt(∂σ∂ε) ≈ m.E/(1+m.ν)/(1-2m.ν)*[1-m.ν m.ν 0; m.ν 1-m.ν 0; 0 0 (1-2m.ν)/2]
+
+end
+
