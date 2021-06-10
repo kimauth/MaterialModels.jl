d5085df66edd896d02120d337a30fb83a04ffd9f
kimauth <auth@chalmers.se>
2021-06-10 19:55:56 +0200

    set up documentation hosting

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, MaterialModels
 
 makedocs(sitename="MaterialModels.jl",  format = Documenter.HTML(prettyurls = false))
+
+deploydocs(
+    repo = "github.com/kimauth/MaterialModels.jl.git",
+)
