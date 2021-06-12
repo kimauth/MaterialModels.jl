b21079635df383bc921ae2696a2fb64aec70714d
Kim Louisa Auth <54391668+kimauth@users.noreply.github.com>
2021-06-12 20:36:03 +0200

    add tests for crystal plasticity (#27)
    * add tests for crystal plasticity
    
    * stop testing on Julia 1.0
    
    * add jld2 tests for crystal plasticity
    
    * use wrapper constructors without parameter in tests
    
    * remove unused code from plastic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using MaterialModels
 using Test
 using Rotations
+using StaticArrays
 using JLD2
-
 
 include("test_utils.jl")
 include("test_linear_elastic.jl")
 include("test_plastic.jl")
+include("test_crystal_visco_plastic.jl")
+include("test_crystal_visco_plastic_red.jl")
 include("test_wrappers.jl")
