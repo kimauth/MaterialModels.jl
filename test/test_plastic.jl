9ee1b7367f192fdc7d5805577b2b8af320f825ba
kimauth <auth@chalmers.se>
2021-06-12 20:29:20 +0200

    remove unused code from plastic test

--- test/test_plastic.jl
+++ test/test_plastic.jl
@@ -27,18 +27,7 @@
     # plastic branch
     Δε = Δε = SymmetricTensor{2,3,Float64}((i,j) -> i==1 && j==1 ? 2ε11_yield : (i == j ? -2ε11_yield*m.ν : 0.0))
     σ, ∂σ∂ε, temp_state = material_response(m, Δε, state; cache=cache)
-
-    # @btime constitutive_driver($m, $Δε, $state; cache=$cache, options=$Dict{Symbol,Any}())
-
-    # σ_trial = state.σ + m.Eᵉ ⊡ Δε
-    # x0 = MaterialModels.Residuals{Plastic}(σ_trial, state.κ, state.α, state.μ)
-    # @btime MaterialModels.residuals($x0, $m, $state, $Δε)
-    # @btime frommandel($(MaterialModels.Residuals{Plastic}), $rand(14))
-    # x_vector = zeros(14)
-    # @btime tomandel!($x_vector, $x0)
-    # @btime MaterialModels.vector_residual!($f, $similar(x_vector), $x_vector, m)
-    # f(x) = MaterialModels.residuals(x, m, state, Δε)
-    # vector_residual!((x->MaterialModels.residuals(x, m, state, zero(SymmetricTensor{2,3}))), r_vector, x_vector, m)
+    @test sqrt(3/2)*norm(dev(σ-temp_state.α)) > m.σ_y
 end
 
 
