6b94c7a07403fa570cc8f9a89ac8760131cba396
Elias <elias.isak.borjesson@gmail.com>
2021-06-16 16:48:59 +0200

    wip traits

--- src/traits.jl
+++ src/traits.jl
@@ -1,33 +1,71 @@
-struct SmallStrain <: StrainMeasure end
-struct GreenLagrangeStrain <: StrainMeasure end
+
+struct GreenLagrange <: StrainMeasure end
 struct RightCauchyGreen <: StrainMeasure end
 struct DeformationGradient <: StrainMeasure end
 struct VelocityGradient <: StrainMeasure end
 
 strainmeasure(::Type{<:AbstractMaterial}) = error("Strain measure for material $m not defined ")
 
-#
-# Main dispatch
-function solid_material_response(m::M, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64, cache, options) where M<:AbstractMaterial 
-    solid_material_response(strainmeasure(M), m, F, state, Δt, cache, options)
+"""
+    material_response_S_dSdE(m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
+
+Function for automatically converting the stress and tangent from any material model to S and dSdE 
+"""
+function material_response_S_dSdE(m::M, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options) where M<:AbstractMaterial 
+    material_response_S_dSdE(strainmeasure(M), m, F, state, Δt, cahce=cache, options=options)
 end
 
 #
 # RightCauchyGreen
-function solid_material_response(::RightCauchyGreen, m::AbstractMaterial, F, state::AbstractMaterialState, Δt::Float64, cache, options)
+function material_response_S_dSdE(::RightCauchyGreen, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
     C = tdot(F)
-    S, dSdE, state = material_response(m, C, state::AbstractMaterialState, Δt::Float64, cache, options)
+    S, dSdC, state = material_response(m, C, stat, Δt, cahce=cache, options=options)
+    return S, 2dSdC, state
+end
+
+#
+# GreenLagrange
+function material_response_S_dSdE(::GreenLagrange, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
+    E = symmetric(1/2 * (F' ⋅ F - one(F)))
+    S, dSdE, state = material_response(m, E, state, Δt, cahce=cache, options=options)
+    return S, dSdE, state
 end
 
 #
 # DeformationGradient
-function solid_material_response(::DeformationGradient, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64, cache, options)
-    S, dSdE, state = material_response(m, F, state::AbstractMaterialState, Δt::Float64, cache, options)
+function material_response_S_dSdE(::DeformationGradient, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
+    Pᵀ, dPᵀdF, state = material_response(m, F, state, Δt, cahce=cache, options=options)
+    S = Pᵀ ⋅ inv(F')
+    dSdE = 2 * inv( otimesu(F,I) ) ⊡ (dPᵀdF - otimesu(S,I)) ⊡ inv( otimesu(F',I) )
+    return S, 2dSdC, state 
+end
+
+"""
+    material_response_Pᵀ_dPᵀdF(m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
+
+Function for automatically converting the stress and tangent from any material model to Pᵀ and dPᵀdF 
+"""
+function material_response_Pᵀ_dPᵀdF(m::M, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options) where M<:AbstractMaterial 
+    material_response_Pᵀ_dPᵀdF(strainmeasure(M), m, F, state, Δt, cache, options)
 end
 
 #
-# SmallStrain
-#function material_response(::DeformationGradientStrain, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64, cache, options)
-#    ε = symmetric(F) - one(F)
-#    σ, dσdε, state = constitutive_driver(m, F, state::AbstractMaterialState, Δt::Float64, cache, options)
-#end
+# DeformationGradient
+function material_response_Pᵀ_dPᵀdF(::DeformationGradient, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
+    Pᵀ, dPᵀdF, state = material_response(m, F, state, Δt, cahce=cache, options=options)
+    return Pᵀ, dPᵀdF, state
+end
+
+#
+# RightCauchyGreen
+function material_response_Pᵀ_dPᵀdF(::RightCauchyGreen, m::AbstractMaterial, F::Tensor{2}, state::AbstractMaterialState, Δt::Float64; cache, options)
+    C = tdot(F)
+    S, dSdC, state = material_response(m, C, state, Δt, cahce=cache, options=options)
+    dPᵀdF =  otimesu(F,I) ⊡ dSdE ⊡ otimesu(F',I) + otimesu(S,I)
+    Pᵀ = S⋅F'
+    return Pᵀ, dPᵀdF, state
+end
+
+#
+# GreenLagrange
+# ...and so on
