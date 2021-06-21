6bc710a7e22f274c7d1848eb3273072acb8de3a3
Knut Andreas Meyer <knutam@gmail.com>
2021-06-21 22:59:36 +0200

    Fixed wrong include file

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using JLD2
 include("test_utils.jl")
 include("test_linear_elastic.jl")
 include("test_plastic.jl")
-include("test_small_strain_plasticity.jl")
+include("test_vonmises_plasticity.jl")
 include("test_crystal_visco_plastic.jl")
 include("test_crystal_visco_plastic_red.jl")
 include("test_wrappers.jl")
