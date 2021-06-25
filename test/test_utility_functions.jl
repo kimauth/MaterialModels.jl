d45464de4e1719729abb8152f86719bd056fcf61
Knut Andreas Meyer <knutam@gmail.com>
2021-06-25 09:02:51 +0200

    Fixed test errors due to using internal functions

--- test/test_utility_functions.jl
+++ test/test_utility_functions.jl
@@ -1,16 +1,16 @@
 @testset "utility functions" begin
     s = rand() + 1.0
     σ = SymmetricTensor{2,3}((i,j)-> i==j && i==1 ? s : 0.0)
-    @test vonmises(σ) ≈ s   # Uniaxial stress should give the correct value
+    @test MaterialModels.vonmises(σ) ≈ s   # Uniaxial stress should give the correct value
 
     σ = SymmetricTensor{2,3}((i,j)-> i==2 && j==1 ? s : 0.0)
-    @test vonmises(σ) ≈ √3*s    # Shear stress have factor √3
+    @test MaterialModels.vonmises(σ) ≈ √3*s    # Shear stress have factor √3
 
     σ = rand(SymmetricTensor{2,3})
-    @test vonmises(σ) ≈ vonmises_dev(dev(σ))    # Check that the deviatoric version works as intended
+    @test MaterialModels.vonmises(σ) ≈ MaterialModels.vonmises_dev(dev(σ))    # Check that the deviatoric version works as intended
 
     x = 1.0 + rand()
-    @test macaulay(-x) ≈ zero(typeof(x))
-    @test macaulay(x) ≈ x
+    @test MaterialModels.macaulay(-x) ≈ zero(typeof(x))
+    @test MaterialModels.macaulay(x) ≈ x
 
 end
